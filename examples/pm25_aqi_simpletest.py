20b9b80cd1dd6835db09df12b8e7c7706fc1e3c2
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-10-20 10:08:33 -0700

    initial repo load

--- examples/pm25_aqi_simpletest.py
+++ examples/pm25_aqi_simpletest.py
@@ -2,18 +2,19 @@
 #
 # SPDX-License-Identifier: Unlicense
 
-from cedargrove_airqualitytools.pm25_aqi import concentration_to_aqi
+from cedargrove_airqualitytools.pm25_aqi import pm25_ppm_to_quality
+from cedargrove_airqualitytools.co2_iaq import co2_ppm_to_quality
 from cedargrove_airqualitytools.translate.english_to_francais import interpret
 
 TRANSLATE = True  # Enable language translator
 
 # Read some sensor values
 PM25_CONCENTRATION = 10  # PM2.5 concentration of 10ppm; GOOD AQI
-CO2_CONCENTRATION = 1450  # CO2 concentration of 1450ppm; HAZARDOUS IAQ
+CO2_CONCENTRATION = 1450  # CO2 concentration of 1450ppm; POOR IAQ
 
 # Calculate the quality and description
 _, aqi_value, _, aqi_desc = pm25_ppm_to_quality(PM25_CONCENTRATION)
-_, iaq_value, -, iaq_desc = co2_ppm_to_quality(CO2_CONCENTRATION)
+_, iaq_value, _, iaq_desc = co2_ppm_to_quality(CO2_CONCENTRATION)
 
 # Print the AQI, description, and translation
 print(f"PM2.5 AQI = {aqi_value} : {aqi_desc}")
